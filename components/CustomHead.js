7b5eef6c862d9c5c7b3ce7c0da73e016badc3ddc
syo <>
2024-02-19 00:01:51 +0900

    微調整

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -6,6 +6,15 @@ const CustomHead = ({ title, description }) => {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width" />
+      <meta name="keywords" content="C2O,WEB,メディア,SEO" />
+      <meta property="og:url" content="https://c2o.co.jp/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:image" content="https://c2o.co.jp/images/share.png" />
+      <meta name="twitter:card" content="summary_large_image" />
       {/* 他の共通メタデータをここに追加 */}
       <link rel="icon" href="/favicon.ico" />
     </Head>
--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,23 @@ const HomePage = () => {
         gsap.registerPlugin(ScrollTrigger); // registerPluginを正しく呼び出す
 
         gsap.to(`.${styles.topBlock}`, {
-        xPercent: -100 * (3 - 1), // 3はアイテム数、1を引くのは0ベースのインデックスのため
+        xPercent: -43 * (4 - 1), // 3はアイテム数、1を引くのは0ベースのインデックスのため
         ease: 'none',
         scrollTrigger: {
             trigger: `.${styles.topSection01}`,
             pin: true,
             scrub: 1,
             end: () => `+=${document.querySelector(`.${styles.topBlock}`).offsetWidth}`,
+            // アニメーション終了時に特定の操作を行う
+            onLeave: () => {
+                // ここにアニメーションが終了した後の処理を追加
+                // 例: .topSection02のスタイルを調整する
+                const nextSection = document.querySelector('.topSection02');
+                if (nextSection) {
+                    nextSection.style.transform = 'translateX(1200px)';
+                }
+            }
+            
         },
         });
     };
@@ -48,13 +58,12 @@ const HomePage = () => {
                 </div>
                 <div className={styles.topRead}>
                     <h3>CONSULTING</h3>
-                    <h4>全ての工程を経験してきたから一貫したご提案が可能です。</h4>
+                    <h4><span>全ての工程を経験してきたから一貫したご提案が可能です。</span></h4>
                     <p>WEB制作におけるすべての工程を経験してきたために<br />
                     企画提案から納品、分析に至るまで一貫したご提案が可能です。<br />
                     また、プロジェクトによって最適な人材をアサインできることが強みです。</p>
                 </div>
                 <section className={styles.topSection01}>
-                    <div className="pageContainer">
                         <div className={styles.topBlock}>
                             <div className={styles.topBlockListItem}>
                                 <figure>
@@ -76,53 +85,49 @@ const HomePage = () => {
                                 <figure>
                                     <Image
                                         src="/images/top_img02.png"
-                                        alt="WEBディレクション"
+                                        alt="メディアコンサルティング"
                                         width={640} // 画像の幅
                                         height={360} // 画像の高さ
                                     />
                                 </figure>
-                                <figcaption className={styles.figcaption}>WEBディレクション</figcaption>
-                                <p>大手広告代理店のグループ会社にて、ナショナルクライアントを中心にWEBディレクターを3年経験。
-                                    その経験を生かして、現在も大手通販メーカーのブランドサイトの企画から構成、
-                                    デザインディレクション、コーディングサポートなど対応中。
-                                    そのほか、企業ポートレートサイトのフルリニューアルや、
-                                    企業サイト制作などWEBサイトに関することでしたら、上流工程から下流工程まで実施可能。</p>
+                                <figcaption className={styles.figcaption}>メディアコンサルティング</figcaption>
+                                <p>月間PV3000万超のニュースメディアの立ち上げから参加し、
+                                    CMS（WordPress）の構築ディレクション、
+                                    広告収益化、SEOを意識したコンテンツ制作、SNS運用、GoogleAnalytics分析などを実施。
+                                    現在もナショナルクライアントのオウンドメディアの全体戦略や、
+                                    CMSのプラグイン提案などの簡単なアドバイスまで、サポートしています。</p>
                             </div>
                             <div className={styles.topBlockListItem}>
                                 <figure>
                                     <Image
                                         src="/images/top_img03.png"
-                                        alt="WEBディレクション"
+                                        alt="運用広告制作"
                                         width={640} // 画像の幅
                                         height={360} // 画像の高さ
                                     />
                                 </figure>
-                                <figcaption className={styles.figcaption}>WEBディレクション</figcaption>
-                                <p>大手広告代理店のグループ会社にて、ナショナルクライアントを中心にWEBディレクターを3年経験。
-                                    その経験を生かして、現在も大手通販メーカーのブランドサイトの企画から構成、
-                                    デザインディレクション、コーディングサポートなど対応中。
-                                    そのほか、企業ポートレートサイトのフルリニューアルや、
-                                    企業サイト制作などWEBサイトに関することでしたら、上流工程から下流工程まで実施可能。</p>
+                                <figcaption className={styles.figcaption}>運用広告制作</figcaption>
+                                <p>現在、大手広告代理店と協力会社としてパートナー契約をしており、
+                                    LP提案、バナー制作、運用レビューなどダイレクトマーケティング分野で6年以上の実績があります。
+                                    また、広告配信パートナー企業と連携して、広告運用も実施可能。
+                                    「自社の広告を出してみたい」「何から始めていいかわからない」など何でもご相談ください！</p>
                             </div>
                             <div className={styles.topBlockListItem}>
                                 <figure>
                                     <Image
                                         src="/images/top_img04.png"
-                                        alt="WEBディレクション"
+                                        alt="SEO対策"
                                         width={640} // 画像の幅
                                         height={360} // 画像の高さ
                                     />
                                 </figure>
-                                <figcaption className={styles.figcaption}>WEBディレクション</figcaption>
-                                <p>大手広告代理店のグループ会社にて、ナショナルクライアントを中心にWEBディレクターを3年経験。
-                                    その経験を生かして、現在も大手通販メーカーのブランドサイトの企画から構成、
-                                    デザインディレクション、コーディングサポートなど対応中。
-                                    そのほか、企業ポートレートサイトのフルリニューアルや、
-                                    企業サイト制作などWEBサイトに関することでしたら、上流工程から下流工程まで実施可能。</p>
+                                <figcaption className={styles.figcaption}>SEO対策</figcaption>
+                                <p>メディア運用、WEBディレクションの実績をもとにアクセス分析やキーワード分析を行います。
+                                すべてのWEB制作物でSEOを意識したディレクトリ構造設計、
+                                サイトスピード設計、コンテンツ制作を行います。</p>
                             </div>
                         </div>
-                        <p className='btn'>私たちができること</p>
-                    </div>
+                        <p className='btn'><a href="/about">私たちができること</a></p>
                     
                 </section>
                 <section className={styles.topSection02}>
@@ -144,7 +149,7 @@ const HomePage = () => {
                                     博報堂グループのWEB制作会社でWEBディレクターとして3年従事し、
                                     ニュースメディア「しらべぇ」の立ち上げに参画。
                                     2018年に合同会社C2Oを設立し、WEB制作やオウンドメディアコンサルタントとして活動。</p>
-                                <p>会社概要</p>
+                                <p className='btn'><a href="/about">会社概要</a></p>
                             </div>
                         </div>
                     </div>
